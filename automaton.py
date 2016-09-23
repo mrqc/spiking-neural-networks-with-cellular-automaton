e211a9d9ed9c3647e64329fe8ab0649fa20dd45e
Erhard Dinhobl <er@master-studios.net>
2016-09-23 11:40:15 +0200

    restructured code

--- automaton.py
+++ automaton.py
@@ -5,6 +5,8 @@ import random
 worldWidth = 20
 worldHeight = 20
 world = [[random.choice([0, 1]) for _1 in range(0, worldWidth)] for _2 in range(0, worldHeight)]
+#world = [[0 for _1 in range(0, worldWidth)] for _2 in range(0, worldHeight)]
+#world[0][0] = world[0][1] = world[0][2] = world[1][0] = world[1][2] = world[2][0] = world[2][2] = 1
 
 def gameOfLifeCell(row, column):
 	global world, worldWidth, worldHeight
@@ -12,22 +14,22 @@ def gameOfLifeCell(row, column):
 	current = world[row][column]
 	for sliceRow in range(row - 1, row + 2):
 		for sliceColumn in range(column - 1, column + 2):
-#			if row == 1 and column == 3:
-#				print sliceRow, sliceColumn, "=", world[sliceRow][sliceColumn]
-			if sliceRow >= 0 and sliceRow < worldHeight and sliceColumn >= 0 and sliceColumn < worldWidth and (row != sliceRow or column != sliceColumn):
-				area.append(world[sliceRow][sliceColumn])
+			if sliceRow >= 0 and sliceRow < worldHeight and sliceColumn >= 0 and sliceColumn < worldWidth:
+				if not (row == sliceRow and column == sliceColumn):
+					area.append(world[sliceRow][sliceColumn])
 	sumArea = sum(area)
-#	if row == 1 and column == 3:
-#		print sumArea
-	if sumArea == 3 and current == 0:
-		return 1
-	if sumArea < 2 and current == 1:
-		return 0
-	if sumArea >= 2 and sumArea <= 3 and current == 1:
-		return 1
-	if sumArea > 3 and current == 1:
-		return 0
-	return current
+	if current == 1:
+		if sumArea < 2:
+			return 0
+		if sumArea >= 2 and sumArea <= 3:
+			return 1
+		if sumArea > 3 and current == 1:
+			return 0
+	elif current == 0:
+		if sumArea == 3:
+			return 1
+		else:
+			return 0
 
 def printWorld():
 	global world, worldWidth, worldHeight
@@ -41,7 +43,7 @@ def printWorld():
 	sys.stdout.flush()
 
 def repaint():
-	sys.stdout.write("\033[F" * worldHeight)
+	sys.stdout.write("\033[F" * (worldHeight))
 	sys.stdout.flush()
 
 def mutate():
@@ -50,11 +52,10 @@ def mutate():
 	for row in range(0, worldHeight):
 		for column in range(0, worldWidth):
 			newWorld[row][column] = gameOfLifeCell(row, column)
-			gameOfLifeCell(row, column)
 	world = newWorld
 			
 while True:
 	printWorld()
 	mutate()
-	sleep(1)
+	sleep(0.2)
 	repaint()
